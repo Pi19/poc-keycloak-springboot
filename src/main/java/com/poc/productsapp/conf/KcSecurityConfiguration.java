0caeeb731ed8a013d7d66b9781b3fb5fad94d1f8
Pi19 <hasimananjara@gmail.com>
2022-06-03 09:31:01 +0200

    conf security URI

--- src/main/java/com/poc/productsapp/conf/KcSecurityConfiguration.java
+++ src/main/java/com/poc/productsapp/conf/KcSecurityConfiguration.java
@@ -11,11 +11,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -88,6 +91,18 @@ public class KcSecurityConfiguration  extends KeycloakWebSecurityConfigurerAdapt
     @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
     public KeycloakRestTemplate keycloakRestTemplate() {
          return new KeycloakRestTemplate(keycloakClientRequestFactory);
+    }
+
+    /**
+     * TODO
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected  void configure (HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.cors().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().authorizeHttpRequests().antMatchers("*").permitAll();
     }
 
 }
